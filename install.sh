447693e5bd4e8c6dddc2e38d6549bfcdf27b660c
Marton Greber <greber.mx@gmail.com>
2022-06-20 12:11:12 +0200

    install sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 # install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#YCM
+
+vim +PlugInstall +qall
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  brew install cmake python go nodejs
+  cd ~/.vim/plugged/YouCompleteMe
+  python3 install.py --all
+# Ubuntu/Debian
+elif [[ -f "/usr/bin/apt-get" ]]; then
+  apt install build-essential cmake python3-dev
+  cd ~/.vim/plugged/YouCompleteMe
+  python3 install.py --all
+fi  
